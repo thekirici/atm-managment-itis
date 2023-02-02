b51975c544a6ded401cb36b66ac04454598ab6dd
thekirici <musa.krc@hotmail.com>
2023-02-03 00:00:48 +0300

    designed.

--- bank managment/src/Login.java
+++ bank managment/src/Login.java
@@ -1,26 +1,73 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Login extends JFrame {
+public class Login extends JFrame implements ActionListener {
 
+    JButton login, signup, clear;
+    JTextField cardTextField;
+    JPasswordField pinTextField;
     Login(){
         setTitle("ATM");
         setLayout(null);
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/banklogo.png"));
-        Image i2 = i1.getImage().getScaledInstance(150,100, Image.SCALE_DEFAULT);
+        Image i2 = i1.getImage().getScaledInstance(120,75, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel label = new JLabel(i3);
-        label.setBounds(70,10,100,100);
+        label.setBounds(50,10,150,100);
         add(label);
 
         JLabel text = new JLabel("Welcome to ATM");
-        text.setBounds(200,40,100,10);
+        text.setFont(new Font("Calibri", Font.BOLD, 38));
+        text.setBounds(220,40,400,40);
         add(text);
+
+        JLabel cardno = new JLabel("Card No:");
+        cardno.setFont(new Font("Calibri", Font.BOLD, 30));
+        cardno.setBounds(80,120,200,30);
+        add(cardno);
+
+        cardTextField = new JTextField();
+        cardTextField.setBounds(240, 120,210,30);
+        cardTextField.setFont(new Font("Arial", Font.BOLD, 18));
+        add(cardTextField);
+
+        JLabel pin = new JLabel("PIN:");
+        pin.setFont(new Font("Calibri", Font.BOLD, 30));
+        pin.setBounds(80,170,400,30);
+        add(pin);
+
+        pinTextField = new JPasswordField();
+        pinTextField.setBounds(240, 170,210,30);
+        pinTextField.setFont(new Font("Arial", Font.BOLD, 18));
+        add(pinTextField);
+
+        login = new JButton("SIGN IN");
+        login.setBounds(240,230,90,35);
+        login.setBackground(Color.BLACK);
+        login.setForeground(Color.WHITE);
+        login.addActionListener(this);
+        add(login);
+
+        clear = new JButton("CLEAR");
+        clear.setBounds(360,230,90,35);
+        clear.setBackground(Color.BLACK);
+        clear.setForeground(Color.WHITE);
+        clear.addActionListener(this);
+        add(clear);
+
+        signup = new JButton("SIGN UP");
+        signup.setBounds(240,280,210,35);
+        signup.setBackground(Color.BLACK);
+        signup.setForeground(Color.WHITE);
+        signup.addActionListener(this);
+        add(signup);
 
         getContentPane().setBackground(Color.WHITE);
 
-        setSize(800,480);
+        setSize(600,480);
         setVisible(true);
         setLocation(300,300);
     }
@@ -29,4 +76,16 @@ public class Login extends JFrame {
     new Login();
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == clear){
+            cardTextField.setText("");
+            pinTextField.setText("");
+        }else if(e.getSource() == login){
+            
+        } else if (e.getSource() == signup) {
+
+        }
+
+    }
 }
